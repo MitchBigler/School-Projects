1e42695c70bf39b15601d82bc675b330d6134917
mitchbigler <mitchelbigler@gmail.com>
2024-10-17 13:12:53 -0600

    Created bits project and test file

---
+++ C++/Project 3 - Bits/testBits.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "bits.h"
+
+using namespace std;
+
+int main() {
+    // Create bits class obj
+    Bits testBits; // Default constructor
+    cout << "Bits object created successfully!" << endl;
+
+    // Example usage of the object
+    cout << "Default bits (should be all zeros): " << testBits << endl;
+
+    // You can test other functions if you implement them later
+    return 0;
+}
